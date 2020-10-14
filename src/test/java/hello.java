5aa678226e8b8a6a695fea1c5fb196509cf41d1f
nida2004 <nyilmaz2025@gmail.com>
2020-10-14 18:48:47 +0200

    conflict secret

--- src/test/java/hello.java
+++ src/test/java/hello.java
@@ -1,6 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello again");
+        System.out.println("conflict line");
         //i am about the right
         //hello
         //second line
